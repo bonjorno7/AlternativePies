628dfb2560a8be325287fea1913f7e570ebe0200
bonjorno7 <jorijndegraaf@gmail.com>
2020-11-16 02:18:06 +0100

    Bump version to 0.0.3
    I know one commit between releases isn't much, but I doubt I'll be touching this addon for a while, and I want the latest commit to be a version change, so here we are.

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ bl_info = {
     'author': 'bonjorno7',
     'description': 'Alternative pie menus to suit my workflow',
     'blender': (2, 80, 0),
-    'version': (0, 0, 2),
+    'version': (0, 0, 3),
     'category': '3D View',
     'location': 'View3D',
 }
