73b8321f04bc011316c1c0d881fdb01473e20ad3
bonjorno7 <jorijndegraaf@gmail.com>
2021-12-25 16:48:38 +0100

    Make keymaps not error when Blender is launched in the background
    When Blender is launched as a background process, register fails because the keyconfig isn't ready at the time of register.
    Because register fails, unregister also fails, since there is no keymap to remove.
    I now only do anything with the keyconfig if it exists, and only do register the keymap if it doesn't exist, and only unregister the keymap if it does exist.
    I figure this means you won't be able to use the shortcuts while Blender is running in the background, but it doesn't matter because... it's running in the background.

--- addon/keymaps/view_3d.py
+++ addon/keymaps/view_3d.py
@@ -6,10 +6,21 @@ keymap = None
 
 def register(keyconfig: bpy.types.KeyConfig):
     global keymap
-    keymap = keyconfig.keymaps.new(name="3D View", space_type="VIEW_3D")
 
-    item = keymap.keymap_items.new('wm.call_menu_pie', 'Z', 'PRESS')
-    item.properties.name = 'ALTPIES_MT_ShadingPie'
+    if keymap is None:
+        keymap = keyconfig.keymaps.new(name="3D View", space_type="VIEW_3D")
 
-    item = keymap.keymap_items.new('wm.call_menu_pie', 'ACCENT_GRAVE', 'PRESS')
-    item.properties.name = 'ALTPIES_MT_ViewPie'
+        item = keymap.keymap_items.new('wm.call_menu_pie', 'Z', 'PRESS')
+        item.properties.name = 'ALTPIES_MT_ShadingPie'
+
+        item = keymap.keymap_items.new('wm.call_menu_pie', 'ACCENT_GRAVE', 'PRESS')
+        item.properties.name = 'ALTPIES_MT_ViewPie'
+
+
+def unregister(keyconfig: bpy.types.KeyConfig):
+    global keymap
+
+    if keymap is not None:
+        keyconfig.keymaps.remove(keymap)
+
+        keymap = None
